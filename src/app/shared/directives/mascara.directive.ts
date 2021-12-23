03989482863dc1fc0f88e220e371d5a61524c1e5
Berkson <berksonx@yahoo.com.br>
2021-12-23 19:08:36 -0300

    criada a diretiva que controla a máscara

---
+++ src/app/shared/directives/mascara.directive.ts
@@ -0,0 +1,82 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Directive({
+  selector: '[mascara]',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: MascaraDirective,
+      multi: true,
+    },
+  ],
+})
+export class MascaraDirective implements ControlValueAccessor {
+  onTouched: any;
+  onChange: any;
+  @Input('mascara') mascara: string; // obtém a máscara
+
+  // el é a referência ao campo input
+  constructor(private el: ElementRef) {
+    this.mascara = '';
+  }
+
+  // primeiro método carregado
+  writeValue(obj: any): void {
+    if (obj) this.el.nativeElement.value = this.aplicarMascara(obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  // sempre que uma tecla for pressionada
+  @HostListener('keyup', ['$event'])
+  onKeyup($event: any) {
+    // substitui tudo que não for número por uma string vazia
+    let valor: string = $event.target.value.replace(/\D/g, '');
+
+    // retorna caso pressionado backspace
+    if ($event.keycode === 8) {
+      this.onChange(valor);
+      return;
+    }
+
+    let pad = this.mascara.replace(/\D/g, '').replace(/9/g, '_');
+    if (valor.length <= pad.length) this.onChange(valor);
+
+    $event.target.value = this.aplicarMascara(valor);
+  }
+
+  @HostListener('blur', ['event'])
+  onBlur($event: any) {
+    if ($event.target.value.length === this.mascara.length) return;
+    this.onChange('');
+    $event.target.value = '';
+  }
+
+  aplicarMascara(valor: string): string {
+    valor = valor.replace(/D/g, '');
+    let pad = this.mascara.replace(/\D/g, '').replace(/9/g, '_');
+    let maskValue = valor + pad.substring(0, pad.length - valor.length);
+    let maskValuePos = 0;
+
+    valor = '';
+
+    for (let i = 0; i < this.mascara.length; i++) {
+      if (isNaN(parseInt(this.mascara.charAt(i)))) {
+        valor += this.mascara.charAt(i);
+      } else {
+        valor += maskValue[maskValuePos++];
+      }
+    }
+
+    if (valor.indexOf('_') > -1) valor = valor.substring(0, valor.indexOf('_'));
+
+    return valor;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MascaraDirective } from './directives';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MascaraDirective],
+  imports: [CommonModule],
+  exports: [MascaraDirective],
 })
-export class SharedModule { }
+export class SharedModule {}
