3e32697031f7751e5175aa12afa4d74f4b2612a2
Berkson <berksonx@yahoo.com.br>
2021-12-23 23:55:09 -0300

    inclusao de modulo

--- src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
+++ src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CadastrarPfComponent } from './components/cadastrar-pf/cadastrar-pf.component';
 import { CadastroPfComponent } from './components/cadastro-pf.component';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [CadastrarPfComponent, CadastroPfComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FlexLayoutModule],
 })
 export class CadastroPfModule {}
