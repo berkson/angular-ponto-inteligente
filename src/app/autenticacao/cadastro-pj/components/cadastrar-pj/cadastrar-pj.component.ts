0a65a1941c89aba6a4e318a83767c70eaaf1e359
Berkson <berksonx@yahoo.com.br>
2021-12-23 21:16:01 -0300

    criado validador de cnpj

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CadastroPj } from '../../models';
-import { CpfValidator } from 'src/app/shared';
+import { CnpjValidator, CpfValidator } from 'src/app/shared';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -30,7 +30,7 @@ export class CadastrarPjComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
-      cnpj: ['', [Validators.required]],
+      cnpj: ['', [Validators.required, CnpjValidator]],
     });
   }
 
